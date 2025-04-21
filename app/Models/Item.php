281eb6a0e5ecc646356ed91baed50ad3676c1537
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-04-21 22:01:18 +0800

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    use HasFactory;
+    
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'item_name',
+        'item_price',
+        'item_quantity',
+    ];
 }
