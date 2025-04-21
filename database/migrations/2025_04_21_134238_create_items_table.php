3cdea1444fbf2cddf8c2ac649c1b43903e4034ab
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-04-21 21:58:10 +0800

    Update 2025_04_21_134238_create_items_table.php

--- database/migrations/2025_04_21_134238_create_items_table.php
+++ database/migrations/2025_04_21_134238_create_items_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('item_name');
+            $table->integer('item_price');
+            $table->integer('item_quantity');
             $table->timestamps();
         });
     }
