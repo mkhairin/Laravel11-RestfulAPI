83518cdb0dc80f8a73f693d627760dc5ea80a623
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-05-06 08:55:23 +0800

    Create  get data function on ItemController.php

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use App\Models\Item;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ItemResource;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    // function menampilkan data
+    public function index() {
+        return ItemResource::collection(Item::all())->additional([
+            'status' => 'success',
+            'code' => 200
+        ]);
+    }
 }
