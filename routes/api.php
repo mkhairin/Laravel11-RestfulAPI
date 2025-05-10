4cddc080257bef727bb2dc5186123f0e02795dd7
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-05-10 08:26:07 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ItemController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/items', ItemController::class);
